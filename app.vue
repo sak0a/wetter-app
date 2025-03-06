875e1ed6cec3efc169dca735a2d5826b88f880d5
Lukenovitzki <121636434+Lukenovitzki@users.noreply.github.com>
2025-03-06 12:35:59 +0100

    Bisschen CSS und HTML

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <index/>
+
   </div>
 </template>
+<script setup lang="ts">
+import Index from "~/pages/index.vue";
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,41 @@
 </script>
 
 <template>
-  <div>
-    <h1>
-      Willkommen zu unserere Wetter App
+  <div class="headerBereich">
+    <h1 class="überschrift">
+      Beste Wetterapp der Welt
     </h1>
+  </div>
+  <div class="main">
+    <section class="content">
+      <article>
+        <h3>Testsection</h3>
+        <svg xmlns="http://www.w3.org/2000/svg"></svg>
+      </article>
+    </section>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="sass">
+
+.headerBereich
+  text-align: center
+  padding: 2rem
+  background: linear-gradient(.25turn, lightblue, white)
+
+.überschrift
+  color: black
+  font-family: Comic Sans MS, sans-serif
+  font-size: 4rem
+  font-weight: bold
+
+.content
+  text-align: center
+  display: flex
+  justify-content: space-evenly
+  flex-wrap: nowrap
+  background-color: blue
+
 
 </style>
 
